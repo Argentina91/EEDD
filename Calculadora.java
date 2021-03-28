2733fe41146a2ffce2b8d4a66d26d0b2eef52830
Argentina Cabello Moreno <argentina.cabello@gmail.com>
2021-03-28 23:14:44 +0200

    Añadido división

--- Calculadora.java
+++ Calculadora.java
@@ -21,8 +21,8 @@ public class Calculadora{
 	System.out.println("  --> Calculadora <--  ");
 	System.out.println("1. Sumar");
 	System.out.println("2. Restar");
-	System.out.println("3.Multiplicar");
-	
+	System.out.println("3. Multiplicar");
+	System.out.println("4. Dividir");
 	System.out.println("5. Salir");
 
 	opcion = entrada.nextInt();
@@ -38,7 +38,10 @@ public class Calculadora{
 		case 3: //Multiplicar
 			resultado=operador1*operador2;
 			break;
-		
+		case 4: //Dividir
+			resultado=(double)operador1/(double)operador2;
+			break;
+		case 5: //Salir
 			break;
 		default: 
 			System.out.println("Seleccione una opcion posible (1-5)");
